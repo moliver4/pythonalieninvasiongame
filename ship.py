fc69d6e920b6436b2cdccd83ed65bbeedf473a9f
moliver4 <tifftang22@gmail.com>
2020-08-06 15:34:35 -0700

    ship class

--- ship.py
+++ ship.py
@@ -11,3 +11,10 @@ clas Ship:
 		# Load the ship image and get its rect.
 		self.image = pygame.image.load('images/ship.bmp')
 		self.rect = self.image.get_rect()
+
+		# Start each new ship at the bottom center of the screen
+		self.rect.midbottom = self.screen_rect.midbottom
+
+	def blitme(sef):
+		"""Draw the ship at its current location"""
+		self.screen.blit(self.image, self.rect)
