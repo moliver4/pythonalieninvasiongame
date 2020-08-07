0bccdf6b856f3e84594b85a89345c33d1be9fb20
moliver4 <tifftang22@gmail.com>
2020-08-07 11:55:28 -0700

    ship moves on keydown to right

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,7 +37,11 @@ class AlienInvasion:
 			elif event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_RIGHT:
 					# Move the ship to the right
-					self.ship.rect.x += 1
+					self.ship.moving_right = True
+			elif event.type == pygame.KEYUP:
+				if event.key == pygame.K_RIGHT:
+					self.ship.moving_right = False
+
 
 	def _update_screen(self):
 		# Redraw screen during each pass through the loop
